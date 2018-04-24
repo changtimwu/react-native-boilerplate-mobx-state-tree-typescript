43ec53beaa7701f9294b07fc61084e981c626f0c
changtimwu <changtimwu@gmail.com>
2018-04-24 17:15:56 +0800

    turn snapshot into behaviour tests

--- src/stories/screens/Login/__tests__/index.test.tsx
+++ src/stories/screens/Login/__tests__/index.test.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import Login from "../index";
-// Note: test renderer must be required after react-native.
-import renderer from "react-test-renderer";
+import Login from "../index"
+import { shallow, ShallowWrapper, configure } from "enzyme"
+import { ButtonProperties } from 'react-native'
+import Adapter from "enzyme-adapter-react-16"
+
+configure({ adapter: new Adapter() });
 
 const onLogin = jest.fn();
 const fillDefault = jest.fn();
@@ -11,7 +14,26 @@ const loginForm = (
 )
 
 
-it("renders correctly", () => {
-	const tree = renderer.create(<Login onLogin={onLogin} loginForm={loginForm} fillDefault={fillDefault} />).toJSON();
-	expect(tree).toMatchSnapshot();
-});
+describe("LoginPage", () => {
+	const w = shallow(<Login onLogin={onLogin} loginForm={loginForm} fillDefault={fillDefault} />)
+	let btn
+	describe("Render", () => {
+		it("login button is there", () => {
+			// <Button block onPress={() => this.props.onLogin()}>
+			let t = w.find('Styled(Button) Styled(Text)').first()
+			expect(t.contains('Login')).toBeTruthy()
+		})
+	})
+	describe('Interaction', () => {
+		it("login button trigs prop func", () => {
+			btn = w.find('Styled(Button)').first()
+			btn.props().onPress()
+			expect(onLogin).toHaveBeenCalledTimes(1)
+		})
+		it("default button trigs prop func", () => {
+			btn = w.find('Styled(Button)').at(1)
+			btn.props().onPress()
+			expect(fillDefault).toHaveBeenCalledTimes(1)
+		})
+	})
+})
